54807c705b62bb549b1c173fb89310a945fffff3
MoisesDavila000 <m.ivan.dr.10@gmail.com>
2023-11-07 18:25:17 -0600

    Practica 7+

--- Practica 7/Practica 7.py
+++ Practica 7/Practica 7.py
@@ -5,30 +5,42 @@ from scipy.stats import mode
 from collections import Counter
 
 ##Obtener puntos X (Oro), Y (Exp) y grupos, crear nuevo df
-##Agrupacion de ORO-EXP dependiendo del desempeño en Micro (Kills, Farms) cuando gana un equipo - Se ignoran los objetivos (Torres, Monstruos de la Jungla)
-def get_points(df, labels, x, y,team=None, obj=False):
+def get_points(df, labels, x, y,team, obj=False):
     new_x = []
     new_y = []
     label = []
-    for data in df.values:
-        if(data[0]==team and data[2]=="No one" and data[3]=="No one" and data[11]<=210 and data[6]<=7 and data[8]==0):
-            new_x.append(data[x])
-            new_y.append(data[y])
-            label.append(labels[0])
-        elif(data[0]==team and data[2]=="No one" and data[3]=="No one" and data[11]>=210 and data[6]>=10 and data[8]==0):
-            new_x.append(data[x])
-            new_y.append(data[y])
-            label.append(labels[2])
-        elif(data[0]==team and data[2]=="No one" and data[3]=="No one"and data[8]==0):
-            new_x.append(data[x])
-            new_y.append(data[y])
-            label.append(labels[1])
+    if(obj==False):
+        ##Agrupacion de ORODIFF - EXPDIFF dependiendo si el equipo gana o pierde
+        for data in df.values:
+            if(data[0]==team):
+                new_x.append(data[x[0]]-data[x[1]])
+                new_y.append(data[y[0]]-data[x[1]])
+                label.append(labels[0])
+            elif(data[0]!=team):
+                new_x.append(data[x[0]]-data[x[1]])
+                new_y.append(data[y[0]]-data[x[1]])
+                label.append(labels[1])
+    else:
+        ##Agrupacion de ORO-EXP dependiendo del desempeño en Micro (Kills, Farms) cuando gana un equipo - Se ignoran los objetivos (Torres, Monstruos de la Jungla)
+        for data in df.values:
+            if(data[0]==team and data[2]=="No one" and data[3]=="No one" and data[11]<=210 and data[6]<=7 and data[8]==0):
+                new_x.append(data[x])
+                new_y.append(data[y])
+                label.append(labels[0])
+            elif(data[0]==team and data[2]=="No one" and data[3]=="No one" and data[11]>=210 and data[6]>=10 and data[8]==0):
+                new_x.append(data[x])
+                new_y.append(data[y])
+                label.append(labels[2])
+            elif(data[0]==team and data[2]=="No one" and data[3]=="No one"and data[8]==0):
+                new_x.append(data[x])
+                new_y.append(data[y])
+                label.append(labels[1])
             
     data = {'Gold': new_x, 'Exp': new_y, 'Group': label}
     new_df = pd.DataFrame(data)
     return new_df
 
-def scatter(path, df, xCol, yCol, labelCol):
+def scatter(path, df, xCol, yCol, labelCol, xlabel, ylabel):
     fig, ax = plt.subplots()
     labels = pd.unique(df[labelCol])
     cmap = get_cmap(len(labels) + 1)
@@ -36,8 +48,8 @@ def scatter(path, df, xCol, yCol, labelCol):
         filter_df = df.query(f"{labelCol} == '{label}'")
         ax.scatter(filter_df[xCol], filter_df[yCol], label = label, color=cmap(i), edgecolors='black', linewidths=1, alpha=0.75)
     ax.legend()
-    plt.xlabel("Oro")
-    plt.ylabel("Exp")
+    plt.xlabel(xlabel)
+    plt.ylabel(ylabel)
     plt.savefig(path)
     plt.close()
 
@@ -76,12 +88,20 @@ def get_tuples(df):
 ##Leer CSV
 df = pd.read_csv("../Practica 2/cleaned_rankedGames.csv")
 new_df = get_points(df, ["Avg-", "Avg", "Avg+"], 9, 10, "Blue", True)
-scatter("Scatter.png", new_df, "Gold", "Exp", "Group")
+scatter("Scatter_MicroPerformance.png", new_df, "Gold", "Exp", "Group", "Oro", "Exp")
 points, labels = get_tuples(new_df)
 for tuples in new_df.itertuples(index=False, name=None):
     points.append((tuples[0], tuples[1]))
     labels.append(tuples[2])
 k_nearest_neightbors(points,labels,[np.array([15000, 15000]),np.array([16000, 17500]),np.array([17500, 18300]), np.array([19300, 18300]), np.array([19000, 19000])],7)
+
+new_df = get_points(df, ["Win", "Lose"], [9, 18], [10, 19], "Blue")
+scatter("Scatter_WinOrLose.png", new_df, "Gold", "Exp", "Group", "Oro Diff", "Exp Diff")
+points, labels = get_tuples(new_df)
+for tuples in new_df.itertuples(index=False, name=None):
+    points.append((tuples[0], tuples[1]))
+    labels.append(tuples[2])
+k_nearest_neightbors(points,labels,[np.array([-5000, -5000]),np.array([-2000, -1500]),np.array([1000, 500]), np.array([4000, 4000]), np.array([7000, 7500])],7)
 
 #Output:
 # Los datos:  [15000 15000]  son del grupo: Avg-
@@ -89,3 +109,8 @@ k_nearest_neightbors(points,labels,[np.array([15000, 15000]),np.array([16000, 17
 # Los datos:  [17500 18300]  son del grupo: Avg
 # Los datos:  [19300 18300]  son del grupo: Avg+
 # Los datos:  [19000 19000]  son del grupo: Avg+
+# Los datos:  [-5000 -5000]  son del grupo: Lose
+# Los datos:  [-2000 -1500]  son del grupo: Lose
+# Los datos:  [1000  500]  son del grupo: Lose
+# Los datos:  [4000 4000]  son del grupo: Win
+# Los datos:  [7000 7500]  son del grupo: Win
